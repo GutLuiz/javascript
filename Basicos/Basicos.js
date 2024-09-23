6280c418feb1f1fef4103db4416af95f675f0c3a
GutLuiz <gutfoda@gmail.com>
2024-09-23 14:21:03 -0300

    Arrays

--- Basicos/Basicos.js
+++ Basicos/Basicos.js
@@ -1,16 +1,39 @@
 
 /*
-VARIAVEIS:
+VARIAVEIL LET:
 */
-//LET
 let idade = 5;
 console.log(idade);
-//CONST: Importante para variaveis que não vão mudar
-const nome = 'rafael';
-console.log (nome)
-//TIPOS PRIMITIVOS:
-let nome1 = 'Rafael'//STRING
-let idadee = 2; // NUMBER
-let aprovado = true; // Booleano
-let sobrenome = undefined // indefinido
-let corSelecionada = null // Redefinir um valor
+
+// VARIAVEL CONST(CONSTANTE): Diferente da let, o valor da variavel é constante (não pode mudar)
+const valorIngressoAdulto = 20;
+console.log(valorIngressoAdulto);
+
+// TIPOS PRIMITIVOS:
+let nome = 'Rafael'; //String
+let idade1 = 25; // number
+let estaAprovado = true; // boolean
+let sobrenome; // Undefined 
+let corSelecionado = null; // Redefinir um valor
+
+// O javascript é uma linguagem totalmente dinamica.
+
+//OBJETOS:
+let pessoa = {
+    nome: 'Rafael'
+};
+console.log(pessoa)
+
+//ARRAYS: conjunto de dados que pode ser acessado por um indice
+//EX:
+let familia = [26,245,50,17];
+//como eu acesso um deles?
+console.log(familia[2]);
+//COMO EU VEJO O TAMANHO DA ARRAY?
+console.log(familia.length);
+
+
+
+
+
+
