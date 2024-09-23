6a3761f4fc08cb7f3d9796bdc00fbb0b85bd5add
GutLuiz <gutfoda@gmail.com>
2024-09-23 14:01:48 -0300

    Variaveis js

--- Basicos/Variaveis.js
+++ Basicos/Variaveis.js
@@ -2,5 +2,15 @@
 /*
 VARIAVEIS:
 */
+//LET
 let idade = 5;
 console.log(idade);
+//CONST: Importante para variaveis que não vão mudar
+const nome = 'rafael';
+console.log (nome)
+//TIPOS PRIMITIVOS:
+let nome1 = 'Rafael'//STRING
+let idadee = 2; // NUMBER
+let aprovado = true; // Booleano
+let sobrenome = undefined // indefinido
+let corSelecionada = null // Redefinir um valor
